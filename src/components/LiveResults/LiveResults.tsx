726c028036e6564bb66184a39e3098453762cfb5
Matt Woods <mattwoodsmaths@gmail.com>
2024-01-15 21:58:23 +0100

    Added Dashboard

--- src/components/LiveResults/LiveResults.tsx
+++ src/components/LiveResults/LiveResults.tsx
@@ -5,6 +5,7 @@ import { calculateErrors, getNetWPM } from "./utils";
 import Acceleration from "../Acceleration";
 import Speedometer from "../Speedometer";
 import SpeedometerTwo from "../SpeedometerTwo";
+import Dashboard from "../DashboardSVGs/Dashboard";
 
 const LiveResults = ({ timeRemaining }: { timeRemaining: number }) => {
   const { typedChars, numberChars, startTime } = useContext(GlobalContext) as GlobalContextType;
@@ -20,18 +21,17 @@ const LiveResults = ({ timeRemaining }: { timeRemaining: number }) => {
 
   return (
     <div className="">
-      <div className="w-[700px] h-28 flex items-center justify-between">
+      {/* <div className="w-[700px] h-28 flex items-center justify-between">
         <div className="border-2 border-slate-500 w-20 py-4 text-center">{numberChars}</div>
         <div className="border-2 border-red-500 w-20 py-4 text-center text-red-400">{errors}</div>
         <div className="border-2 border-purple-500 w-20 py-4 text-center text-purple-400">
           {getNetWPM(typedChars.length, elapsedTime, errors)}
         </div>
-        {/* <Acceleration /> */}
         <div className="border-2 border-slate-500 w-20 py-4 text-center">{timeRemaining}</div>
-      </div>
+      </div> */}
       <div className="mx-auto p-10 flex items-center justify-center">
-        {/* <Speedometer /> */}
         <SpeedometerTwo typedChars={typedChars} elapsedTime={elapsedTime} errors={errors} />
+        <div className=""></div>
       </div>
     </div>
   );
--- src/components/SpeedometerTwo.tsx
+++ src/components/SpeedometerTwo.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getNetWPM } from "./LiveResults/utils";
+import Dashboard from "./DashboardSVGs/Dashboard";
 
 const SpeedometerTwo = ({
   typedChars,
@@ -32,48 +33,35 @@ const SpeedometerTwo = ({
   }, [typedChars, elapsedTime, errors]);
 
   return (
-    <svg height={350} width={350}>
-      ‍
-      <circle
-        cx={180}
-        cy={170}
-        fill="transparent"
-        r={innerRadius}
-        transform={`rotate(135 ${150} ${150}) translate(0 -60)`}
-        stroke="black"
-        strokeLinecap="butt"
-        strokeWidth={strokeWidth}
-        strokeDasharray={dashArray}
-      />
-      <defs>
-        <linearGradient id="grad" x1="0" y1="0" x2="1" y2="1">
-          <stop offset="0.0408604" stopColor="#F06565" />
-          <stop offset="0.436563" stopColor="#DD2FCD" />
-          <stop offset="1" stopColor="#3827FF" />
-        </linearGradient>
-        <filter id="f1" x="0" y="0" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_10_2" />
-        </filter>
-      </defs>
-      <circle
-        cx={180}
-        cy={170}
-        fill="transparent"
-        r={innerRadius}
-        transform={`rotate(135 ${150} ${150}) translate(0 -60)`}
-        stroke="url(#grad)"
-        strokeLinecap="butt"
-        style={{
-          transition: "stroke-dashoffset 0.5s ease-in-out",
-        }}
-        // stroke="hotpink"
-        strokeWidth={strokeWidth}
-        strokeDasharray={dashArray}
-        strokeDashoffset={percentage}
-      />
-      <g filter="url(#f1)">
+    <div className=" relative w-[700px] h-[400px]">
+      <div className="absolute left-0 top-0 translate-y-[-20%] translate-x-[-8%]">
+        <Dashboard />
+      </div>
+      <svg height={350} width={350}>
+        ‍
+        <circle
+          cx={180}
+          cy={170}
+          fill="transparent"
+          r={innerRadius}
+          transform={`rotate(135 ${150} ${150}) translate(0 -60)`}
+          stroke="black"
+          strokeLinecap="butt"
+          strokeWidth={strokeWidth}
+          strokeDasharray={dashArray}
+        />
+        <defs>
+          <linearGradient id="grad" x1="0" y1="0" x2="1" y2="1">
+            <stop offset="0.0408604" stopColor="#F06565" />
+            <stop offset="0.436563" stopColor="#DD2FCD" />
+            <stop offset="1" stopColor="#3827FF" />
+          </linearGradient>
+          <filter id="f1" x="0" y="0" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+            <feGaussianBlur stdDeviation="10" result="effect1_foregroundBlur_10_2" />
+          </filter>
+        </defs>
         <circle
           cx={180}
           cy={170}
@@ -90,8 +78,26 @@ const SpeedometerTwo = ({
           strokeDasharray={dashArray}
           strokeDashoffset={percentage}
         />
-      </g>
-    </svg>
+        <g filter="url(#f1)">
+          <circle
+            cx={180}
+            cy={170}
+            fill="transparent"
+            r={innerRadius}
+            transform={`rotate(135 ${150} ${150}) translate(0 -60)`}
+            stroke="url(#grad)"
+            strokeLinecap="butt"
+            style={{
+              transition: "stroke-dashoffset 0.5s ease-in-out",
+            }}
+            // stroke="hotpink"
+            strokeWidth={strokeWidth}
+            strokeDasharray={dashArray}
+            strokeDashoffset={percentage}
+          />
+        </g>
+      </svg>
+    </div>
   );
 };
 
